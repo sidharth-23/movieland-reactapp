25bfc1d4e79737314b9f231b308a7b6d39fac830
Sidharth C <132810326+sidharth-23@users.noreply.github.com>
2023-10-07 15:01:50 +0530

    Update App.js
    the api was served over a http connection and not a https one. this made it not possible for the api to fetch the movie details. Now the issue is fixed and i believe the app will work just fine.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import SearchIcon from './search.svg'
 //668803e8
 
-const API_URL = 'http://www.omdbapi.com?apikey=668803e8';
+const API_URL = 'https://www.omdbapi.com?apikey=668803e8';
 
 // const movie1 =
 // {
